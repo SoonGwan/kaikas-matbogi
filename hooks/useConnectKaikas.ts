12cd15a61d7f331a7d3f9ccd35a050d604264f07
권순관 <reactsaranghe@naver.com>
2022-06-13 21:59:18 +0900

    feat: enable klaytn

--- hooks/useConnectKaikas.ts
+++ hooks/useConnectKaikas.ts
@@ -1,21 +1,24 @@
+import { AbstractCaver } from 'caver-js';
 import { useCallback, useState } from 'react';
 import { useHealthCheck } from './useHealthCheck';
 
-export const useConnectKaikas = (caver: any) => {
+export const useConnectKaikas = (caver: AbstractCaver) => {
   const { isUnlocked } = useHealthCheck();
   const [address, setAddress] = useState('');
   const [balance, setBalance] = useState('');
 
   const handleConnect = useCallback(async () => {
-    if (isUnlocked) {
-      const result = await caver.klay.getAccounts();
+    window.klaytn.enable().then(async () => {
+      if (isUnlocked) {
+        const result = await caver.klay.getAccounts();
 
-      const balance = Number(await caver.klay.getBalance(result[0]));
-      const convertBalance = caver.utils.convertFromPeb(balance, 'KLAY');
+        const balance = Number(await caver.klay.getBalance(result[0]));
+        const convertBalance = caver.utils.convertFromPeb(balance, 'KLAY');
 
-      setBalance(convertBalance);
-      setAddress(result[0]);
-    }
+        setBalance(convertBalance);
+        setAddress(result[0]);
+      }
+    });
   }, [caver.klay, caver.utils, isUnlocked]);
 
   return { handleConnect, address, balance };
